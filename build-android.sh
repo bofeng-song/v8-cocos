9150e5436416fc2dfafe90bab5a02731ecf1818d
bofeng <bofeng.song@chukong-inc.com>
2025-07-07 10:05:37 +0800

    ASS

--- build-android.sh
+++ build-android.sh
@@ -41,7 +41,6 @@ v8_enable_i18n_support=false
 treat_warnings_as_errors=false
 symbol_level=0
 v8_enable_webassembly=true
-use_cxx17=true
 clang_version=\"19\"
 v8_enable_sandbox=false
 android_ndk_root=\"${NDK_ROOT_R21}\"
